85abe617ef3cba3d9aa6e16f3197fdcba6e16306
Jose Mojica <45056162+MejiMM@users.noreply.github.com>
2022-07-01 12:40:20 -0300

    Update about.component.ts

--- frontend/src/app/components/about/about.component.ts
+++ frontend/src/app/components/about/about.component.ts
@@ -1,73 +1,72 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Educacion } from 'src/app/services/educacion.model';
 import { LoginService } from 'src/app/services/login.service';
 import { PortfolioService } from 'src/app/services/portfolio.service';
-import { User } from 'src/app/services/user.model';
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  selector: 'app-education',
+  templateUrl: './education.component.html',
+  preserveWhitespaces: false,
+  styleUrls: ['./education.component.css']
 })
-export class AboutComponent implements OnInit {
+export class EducationComponent implements OnInit {
+  
+  educacions :any = [];
+  isLogged :boolean = false;
+  form :FormGroup;  
+  formEd :boolean = false;
+  loading :boolean = false;
+  
+  constructor(private portfolioServ :PortfolioService, private login :LoginService, private formBuilder :FormBuilder) {
 
-  private isLogged: boolean = false;
-  user!: User;
-  private isEditEnabled: boolean = false;
-  descripcionUser!: String;
-  loading?: boolean = true;
+    //Bring the elements of education to be displayed
+    this.portfolioServ.getEducacion().subscribe( e => {
 
-  constructor(private portfolioServ: PortfolioService, private login: LoginService) {
+      this.educacions = e; 
+    });
 
-    this.login.currentLoaderSubject.asObservable().subscribe(e => this.loading = e);
+    //The constructor for the form fields
+    this.form = this.formBuilder.group({
+      lugar: ["", Validators.required],
+      titulo: ["", Validators.required],
+      urlCert: [""]
+    });
 
+    //Detect changes in the token to render the portfolio edition
     this.login.getSession().subscribe(e => {
-      if (e.tokenDeAcceso) {
-        this.isLogged = true
-      };
-    })
-  }
-
-  ngOnInit(): void {
-    //Bring the user data to later show in the template
-    this.portfolioServ.getUser().subscribe(e => {
-      this.descripcionUser = e[0].descripcion;
-
-      this.user = {
-        nombre: e[0].nombre,
-        apellido: e[0].apellido,
-        descripcion: e[0].descripcion
+      if(e.tokenDeAcceso) {
+        this.login.currentLoaderSubject.asObservable().subscribe(e => this.loading = e);
+        this.isLogged = true;
       }
     });
   }
+  
+  ngOnInit(): void { }
 
-  editDescription() {
-    this.isEditEnabled = !this.isEditEnabled;
+  //Change the edition of the education section according to the user who is logged in
+  toggleFormEd() {
+    this.formEd = !this.formEd;    
   }
 
-  getIsEditEnabled() {
-    return this.isEditEnabled;
-  }
+  addEducacion() {
 
-  getIsLogged() {
-    return this.isLogged;
-  }
-
-  editDescripcion() {
-    this.isEditEnabled = !this.isEditEnabled;
-
-    const user: User = {
-      nombre: this.user.nombre,
-      apellido: this.user.apellido,
-      descripcion: this.descripcionUser
+    const educacion :Educacion = {
+      lugar: this.form.get("lugar")?.value,
+      titulo: this.form.get("titulo")?.value,
+      urlCert: this.form.get("urlCert")?.value
     }
 
-    this.portfolioServ.editUser(9, user).subscribe(() => {
-      this.user = {
-        nombre: user.nombre,
-        apellido: user.apellido,
-        descripcion: user.descripcion
-      };
+    this.formEd = false;
+
+    this.portfolioServ.addEd(educacion).subscribe( e => { 
+      this.educacions.push(e);
     });
+
+  }
+
+  deleteEducacion(id :number) :void {
+    this.portfolioServ.deleteEd(id).subscribe( e => this.educacions = e);
   }
 
 }
